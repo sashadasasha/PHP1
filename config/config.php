dd1f6e3a335b655bc1ffa587859cce140015403c
Sasha Korn <you@example.com>
2019-08-18 20:02:48 +0300

    lesson4

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+define("TEMPLATES_DIR", $_SERVER['DOCUMENT_ROOT']."/views/");
+
+include_once $_SERVER['DOCUMENT_ROOT']."/engine/functions.php";
---
+++ views/gallery.php
@@ -0,0 +1,9 @@
+Галерея<br>
+<div class="gallery">
+<?foreach ($images as $image):?>
+
+<img src ="<?=$addres . $image?>" id="<?=$image?>">
+
+<?endforeach;?> 
+</div>
+<div class="modalWindow"></div>
